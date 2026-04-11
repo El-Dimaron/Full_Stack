b05b4c9597033e8b1f068033d6264b3920867ea3
El-Dimaron <el.dimaron.n1@gmail.com>
2026-04-11 17:13:52 +0300

    Lesson 24

--- Notes/js_practise/main_11.js
+++ Notes/js_practise/main_11.js
@@ -1,20 +1,23 @@
-function countBrackets(data) {
-  if (!data || !data.includes(")")) {
-    return false;
+function raffleOdds(totals, purchased) {
+  let loseNum = 1;
+  let loseDen = 1;
+
+  for (let i = 0; i < totals.length; i++) {
+    loseNum = loseNum * (totals[i] - purchased[i]);
+    loseDen = loseDen * totals[i];
   }
 
-  let openBrackets = 0,
-    closeBrackets = 0;
-  for (let i of data) {
-    if (i === "(") {
-      openBrackets += 1;
-    } else if (i === ")") {
-      closeBrackets += 1;
-    }
-  }
-  return openBrackets === closeBrackets;
+  console.log(loseNum);
+  console.log(loseDen);
+
+  let winNum = loseDen - loseNum;
+  let winDen = loseDen;
+
+  return `${winNum}/${winDen}`;
 }
 
-console.log(countBrackets("(( ))"));
-console.log(countBrackets("(( )"));
-console.log(countBrackets("abc ) def ( 123 @#$ ()"));
+console.log(raffleOdds([4, 4], [1, 1]));
+console.log(raffleOdds([2, 3, 6], [1, 1, 1]));
+
+// 1/2 * 1/3 * 1/6
+// 1/2 * 2/3 * 5/6
