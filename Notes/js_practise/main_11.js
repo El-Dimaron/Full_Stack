0e298a9b4a82edb8fa172221289d011365baa06a
El-Dimaron <el.dimaron.n1@gmail.com>
2026-04-19 15:51:52 +0300

    Lesson 25/26

--- Notes/js_practise/main_11.js
+++ Notes/js_practise/main_11.js
@@ -1,23 +1,15 @@
-function raffleOdds(totals, purchased) {
-  let loseNum = 1;
-  let loseDen = 1;
+console.log(Window);
+const currentUrl = window.location.href;
+console.log(`Поточний URL: ${currentUrl}`);
 
-  for (let i = 0; i < totals.length; i++) {
-    loseNum = loseNum * (totals[i] - purchased[i]);
-    loseDen = loseDen * totals[i];
-  }
+// Зміна шляху на поточній сторінці (без перезавантаження)
+// window.location.pathname = "/new-path";
 
-  console.log(loseNum);
-  console.log(loseDen);
+// Додавання параметрів до URL
+// window.location.search = "?query=новий_запит";
 
-  let winNum = loseDen - loseNum;
-  let winDen = loseDen;
+// Перенаправлення на нову сторінку
+// window.location.href = 'https://example.com';
 
-  return `${winNum}/${winDen}`;
-}
-
-console.log(raffleOdds([4, 4], [1, 1]));
-console.log(raffleOdds([2, 3, 6], [1, 1, 1]));
-
-// 1/2 * 1/3 * 1/6
-// 1/2 * 2/3 * 5/6
+// Перезавантаження поточної сторінки
+// window.location.reload();
